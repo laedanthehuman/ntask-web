136a2ed0fb0b2855803af342213bb5f7091aaa71
Laedan The Human <manoel.josefneto@gmail.com>
2016-06-21 23:14:15 -0300

    Template e component de cadstro criados

---
+++ src/templates/signup.js
@@ -0,0 +1,25 @@
+exports.render = () =>{
+    return `<form >
+    <div class="list">
+        <label class="item item-input item-stacked-label">
+            <span class="input-text">Nome</span>
+            <input type="text" data-name>
+        </label>
+        <label class="item item-input item-stacked-label">
+            <span class="input-text">Email</span>
+            <input type="text" data-email>
+        </label>
+        <label class="item item-input item-stacked-label">
+            <span class="input-text">Senha</span>
+            <input type="password" data-password>
+        </label>
+    </div>
+</form>  
+
+<div class="padding">
+    <button class="button button-positive button-block">
+        <i class="ion-thumbsup"></i>Cadastrar
+    </button>
+</div>
+    `   
+}
